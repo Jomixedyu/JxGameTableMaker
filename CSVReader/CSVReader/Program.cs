3ec24a4560e72c4dac72e241b7438c1cbd2a77bd
Jayshonyves <j@imxqy.com>
2019-08-16 13:34:56 +0800

    ttt
    ttt

---
+++ CSVReader/CSVReader/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSVReaderP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSVReader.CSV2DataTable(@"""asd""str""dsa""");
+            Console.ReadKey();
+        }
+    }
+}
